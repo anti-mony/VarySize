e35eb7338d820daa07c494ed815e657c78ce4d70
Sushant Bansal <1star.sushant@gmail.com>
2015-12-29 16:14:24 +0530

    Fixed a few logical errors

--- app/src/main/java/com/beforesup/varysize/MainActivity.java
+++ app/src/main/java/com/beforesup/varysize/MainActivity.java
@@ -1,7 +1,7 @@
 package com.beforesup.varysize;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,24 +12,25 @@ public class MainActivity extends AppCompatActivity {
     float size = 25;
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         Button uP = (Button) findViewById(R.id.UP);
         Button dOWN = (Button) findViewById(R.id.DOWN);
         final EditText hY = (EditText) findViewById(R.id.HY);
         uP.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View V) {
-                if(size!=85)
-                    hY.setTextSize(size += 5);
-                else
-                    Toast.makeText(getApplicationContext(),"Max Size Reached",Toast.LENGTH_SHORT).show();
+                if (size < 85) {
+                    size += 5;
+                    hY.setTextSize(size);
+                } else
+                    Toast.makeText(getApplicationContext(), "Size Limit Reached", Toast.LENGTH_SHORT).show();
             }
         });
         dOWN.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View V) {
-                hY.setTextSize(size -= 5);
+                size -= 5;
+                hY.setTextSize(size);
             }
         });
     }
